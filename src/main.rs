97cc80e5ae495db652bd768fa0e9bcbca178c794
Shane <marusczaks@gmail.com>
2021-11-18 21:22:04 -0500

    negative number support

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
 use std::collections::HashMap;
+use std::io;
 
-fn main() {}
+fn main() {
+    println!("Number text to integer");
+    let mut name = String::new();
 
-fn get_power(num: u32) -> u32 {
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+
+    let num = text_to_int(&name);
+
+    println!("{}", num);
+}
+
+fn get_power(num: usize) -> u32 {
     if num == 0 {
         2
     } else {
-        num * 3
+        (num * 3) as u32
     }
 }
 
-fn text_to_int(text_num: &str) -> usize {
-    let mut num_words: HashMap<String, (usize, usize)> = HashMap::new();
+fn text_to_int(text_num: &str) -> isize {
+    let mut num_words: HashMap<String, (isize, isize)> = HashMap::new();
 
     let units = [
         "zero",
@@ -44,38 +56,41 @@ fn text_to_int(text_num: &str) -> usize {
 
     num_words.insert(String::from("and"), (1, 0));
 
-    for (idx, word) in units.iter().enumerate() {
-        num_words.insert(String::from(*word), (1, idx));
+    for (index, word) in units.iter().enumerate() {
+        num_words.insert(String::from(*word), (1, index as isize));
     }
 
-    for (idx, word) in tens.iter().enumerate() {
-        num_words.insert(String::from(*word), (1, idx * 10));
+    for (index, word) in tens.iter().enumerate() {
+        num_words.insert(String::from(*word), (1, index as isize * 10));
     }
 
     let num: usize = 10;
 
-    for (idx, word) in scales.iter().enumerate() {
-        num_words.insert(String::from(*word), (num.pow(get_power(idx as u32)), 0));
+    for (index, word) in scales.iter().enumerate() {
+        num_words.insert(String::from(*word), (num.pow(get_power(index)) as isize, 0));
     }
 
     let mut current = 0;
 
     let mut result = 0;
 
+    let mut multipler = 1;
+
     for word in text_num.split_whitespace() {
-        if !num_words.contains_key(word) {
+        if word.eq("negative") {
+            multipler = -1;
+        } else if !num_words.contains_key(word) {
             panic!("Invalid word")
-        }
+        } else {
+            let (scale, increment) = num_words[word];
 
-        let (scale, increment) = num_words[word];
-
-        current = current * scale + increment;
-
-        if scale > 100 {
-            result += current;
-            current = 0;
+            current = current * scale + increment;
+            if scale > 100 {
+                result += current;
+                current = 0;
+            }
         }
     }
 
-    result + current
+    (result + current) * multipler
 }
