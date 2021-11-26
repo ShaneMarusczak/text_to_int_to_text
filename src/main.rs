beddc02aae0ad1cbc92b8de8f3903e412124acd1
Shane <marusczaks@gmail.com>
2021-11-26 13:35:38 -0500

    rewrite to immediately accept text or int inputs

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,48 @@ use std::io;
 use to_int_and_back::to;
 
 fn main() {
-    println!("Number text to integer");
+    println!("Number Conversion");
+
     let mut name = String::new();
 
     io::stdin()
         .read_line(&mut name)
         .expect("Failed to read line");
-
-    // let num = to::int(&name);
-
-    let i: isize = match name.trim().parse() {
-        Ok(num) => num,
-        Err(_) => panic!("AHHHH"),
+    match name.trim().parse::<isize>() {
+        Ok(num) => println!("{}", to::string(num)),
+        Err(_) => to_int(&name),
     };
+}
 
-    let num = to::string(i);
+fn to_int(old_name: &str) {
+    let mut new_name = String::from(old_name);
+    let mut retry = false;
+    loop {
+        if retry {
+            io::stdin()
+                .read_line(&mut new_name)
+                .expect("Failed to read line");
+        }
+        match to::int(&new_name.trim()) {
+            Ok(n) => {
+                println!("{}", n);
+                break;
+            }
+            Err(e) => {
+                println!("{}", e);
+                println!("Try Again? y/n");
+                new_name.clear();
 
-    println!("{}", num);
+                io::stdin()
+                    .read_line(&mut new_name)
+                    .expect("Failed to read line");
+                if new_name.trim().eq("y") {
+                    retry = true;
+                    new_name.clear();
+                } else {
+                    break;
+                }
+            }
+        }
+    }
 }
